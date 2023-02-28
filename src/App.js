68c44133a92e0c240cb8ec9a5f5c9db632035316
Adrian Chapa <achapa@Adrians-MacBook-Pro.local>
2023-02-28 03:59:17 -0600

    before breaking into sep components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filterName, setFilterName] = useState("");
+
+  console.log(filterName);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // add new person to phonebook
+    if (persons.map((person) => person.name).includes(newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons((prevState) => [
+        ...prevState,
+        { name: newName, number: newNumber },
+      ]);
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.includes(filterName)
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <div>
+        filter shown with:{" "}
+        <input
+          value={filterName}
+          onChange={(e) => setFilterName(e.target.value)}
+        />
+      </div>
+      <h2>add a new</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{" "}
+          <input
+            onChange={(e) => setNewName(e.target.value)}
+            value={newName}
+          />
+        </div>
+        <div>
+          number:{" "}
+          <input
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {filterName
+        ? filteredPersons.map((person) => (
+            <li key={person.name}>
+              {person.name} {person.number}
+            </li>
+          ))
+        : persons.map((person) => (
+            <li key={person.name}>
+              {person.name} {person.number}
+            </li>
+          ))}
     </div>
   );
-}
+};
 
 export default App;
