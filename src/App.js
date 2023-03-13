2ce46812af9f7416a8702b08e4885714158fcac7
Adrian Chapa <achapa@Adrians-MacBook-Pro.local>
2023-03-13 00:48:30 -0500

    Filter works for upper and lower case

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
   };
 
   const filteredPersons = persons.filter((person) =>
-    person.name.includes(filterName)
+    person.name.toLowerCase().includes(filterName.toLowerCase())
   );
 
   return (
